1030390b9b73fd1239dec4d673bc254abee3212a
Drew Fout <drewmf1566@gmail.com>
2024-06-30 14:29:24 -0500

    click on login button goes to /login

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import LoginFormPage from "../LoginFormPage/LoginFormPage";
+
 
 function Navigation() {
+  const sessionUser = useSelector(state => state.session.user)
+  const navigate = useNavigate()
+
+  const handleLogin = ()=>{
+    navigate('/login')
+  }
+
   return (
     <div class="navbar">
         <div class="logo">HeapLeak</div>
@@ -10,12 +20,19 @@ function Navigation() {
             <p>About</p>
             <input type="search" placeholder="search" />
         </div>
-        <div class="login">
-            <button type="submit">Login</button>
-            <div class="signup">
-                <button type="submit">Sign Up</button>
-            </div>
-        </div>
+        {!sessionUser && 
+                (<div class="login">
+                <button onClick={handleLogin}>Login</button>
+                <div class="signup">
+                    <button type="submit">Sign Up</button>
+                </div>
+            </div>)
+        
+        }
+        {sessionUser && 
+        (                    <li id= 'profile'>
+          <ProfileButton user={sessionUser} className='profile-button' />
+      </li>)}
     </div>
   );
 }
