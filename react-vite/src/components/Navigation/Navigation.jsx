886f602dc8f1cc6dcdb5ecf04c1ac8b8da5c0127
Drew Fout <drewmf1566@gmail.com>
2024-06-30 15:46:34 -0500

    click signup button goes to /signup

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,34 +1,38 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-import LoginFormPage from "../LoginFormPage/LoginFormPage";
+
 
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user)
   const navigate = useNavigate()
 
-  const handleLogin = ()=>{
+  const goToLogin = ()=>{
     navigate('/login')
   }
 
   const goHome = () => {
     navigate('/')
   }
+  const goToSignup = () => {
+    navigate('/signup')
+  }
 
   return (
     <div className="navbar">
         <div onClick={goHome}className="logo">HeapLeak</div>
         <div className="search">
-            <p>About</p>
+            <Link>About</Link>
             <input type="search" placeholder="search" />
         </div>
         {!sessionUser && 
                 (<div className="login">
-                <button onClick={handleLogin}>Login</button>
+                <button onClick={goToLogin}>Login</button>
                 <div className="signup">
-                    <button type="submit">Sign Up</button>
+                    <button onClick={goToSignup}>Sign Up</button>
                 </div>
             </div>)
         
