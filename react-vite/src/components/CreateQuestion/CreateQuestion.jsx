54a1c65ef74bbaf58a05ce6abccf816bf09e764f
withdrw <finesse@usa.com>
2024-07-02 13:10:30 -0500

    fix tags

--- react-vite/src/components/CreateQuestion/CreateQuestion.jsx
+++ react-vite/src/components/CreateQuestion/CreateQuestion.jsx
@@ -3,36 +3,69 @@
 // import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./CreateQuestion.css";
+import { useState } from "react";
+// import { createQuestionThunk } from "../../redux/question";
+import { enumTags } from "./tags";
 
 const CreateQuestion = () => {
-  // const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [tags,setTags] = useState([])
 
-  const handleDiscard = () =>{
-    navigate('/')
-  }
+
+  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleDiscard = () => {
+    navigate("/");
+  };
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const question = {
+      title,
+      body,
+      tags,
+    };
+    // dispatch(CreateQuestion(question));
+    console.log(question)
+    setTitle("");
+    setBody("");
+    setTags([]);
+  };
+
 
   return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="question-form">
+        <h2>Create a Question</h2>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <label htmlFor="problem">Problem:</label>
+        <textarea id="problem" name="problem" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <div className="tags">
+          <h3>Tags:</h3>
+          <ul id="tag-list">
+            {Object.values(enumTags).map((tag) => (
+              <li key={tag}>
+                <input type="checkbox"
+                />
+                <label>{tag}</label>
+              </li>
+            ))}
 
-
-  <div className="container">
-    <form className="question-form">
-      <h2>Create a Question</h2>
-      <label htmlFor="title">Title:</label>
-      <input type="text" id="title" name="title" />
-      <label htmlFor="problem">Problem:</label>
-      <textarea id="problem" name="problem"></textarea>
-      <label htmlFor="attempt">Attempt:</label>
-      <input type="text" id="attempt" name="attempt" />
-      <div className="tags">
-        <h3>Tags:</h3>
-        <input type="text" id="tag-input" placeholder="Add a tag" />
-        <button type="button" id="add-tag">Add</button>
-        <ul id="tag-list"></ul>
-      </div>
-      <button type="button" onClick={handleDiscard}id="discard-btn">Discard</button>
-    </form>
-  </div>
+          </ul>
+        </div>
+        <button type="button" onClick={handleDiscard} id="discard-btn">
+          Discard
+        </button>
+        <button type="submit" id="discard-btn">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
